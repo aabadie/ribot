686c4265479090cc027dceee57c54527262e51c5
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-02-18 21:55:13 +0100

    extend CoAP api

--- coap-fw/coap.c
+++ coap-fw/coap.c
@@ -18,6 +18,8 @@
 #include "debug.h"
 #define ENABLE_DEBUG (1)
 
+#define APPLICATION_NAME "RiBot"
+
 #define SPEED_FACTOR 0.8
 #define ANGLE_FACTOR 0.5
 
@@ -33,6 +35,16 @@ static int handle_get_well_known_core(coap_rw_buffer_t *scratch,
                                       const coap_packet_t *inpkt,
                                       coap_packet_t *outpkt,
                                       uint8_t id_hi, uint8_t id_lo);
+
+static int handle_get_name(coap_rw_buffer_t *scratch,
+                           const coap_packet_t *inpkt,
+                           coap_packet_t *outpkt,
+                           uint8_t id_hi, uint8_t id_lo);
+
+static int handle_get_os(coap_rw_buffer_t *scratch,
+                         const coap_packet_t *inpkt,
+                         coap_packet_t *outpkt,
+                         uint8_t id_hi, uint8_t id_lo);
 
 static int handle_get_board(coap_rw_buffer_t *scratch,
                             const coap_packet_t *inpkt,
@@ -57,6 +69,12 @@ static int handle_put_ribot(coap_rw_buffer_t *scratch,
 static const coap_endpoint_path_t path_well_known_core =
 { 2, { ".well-known", "core" } };
 
+static const coap_endpoint_path_t path_name =
+{ 1, { "name" } };
+
+static const coap_endpoint_path_t path_os =
+{ 1, { "os" } };
+
 static const coap_endpoint_path_t path_board =
 { 1, { "board" } };
 
@@ -70,6 +88,10 @@ const coap_endpoint_t endpoints[] =
 {
     { COAP_METHOD_GET,	handle_get_well_known_core,
       &path_well_known_core, "ct=40" },
+    { COAP_METHOD_GET,	handle_get_name,
+      &path_name,	   "ct=0"  },
+    { COAP_METHOD_GET,	handle_get_os,
+      &path_os,  	   "ct=0"  },
     { COAP_METHOD_GET,	handle_get_board,
       &path_board,	   "ct=0"  },
     { COAP_METHOD_GET,	handle_get_mcu,
@@ -132,6 +154,36 @@ static int handle_get_well_known_core(coap_rw_buffer_t *scratch,
                               COAP_CONTENTTYPE_APPLICATION_LINKFORMAT);
 }
 
+static int handle_get_name(coap_rw_buffer_t *scratch,
+                           const coap_packet_t *inpkt,
+                           coap_packet_t *outpkt,
+                           uint8_t id_hi, uint8_t id_lo)
+{
+    const char *app_name = APPLICATION_NAME;
+    size_t len = strlen(APPLICATION_NAME);
+
+    memcpy(response, app_name, len);
+
+    return coap_make_response(scratch, outpkt, (const uint8_t *)response, len,
+                              id_hi, id_lo, &inpkt->tok, COAP_RSPCODE_CONTENT,
+                              COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_get_os(coap_rw_buffer_t *scratch,
+                         const coap_packet_t *inpkt,
+                         coap_packet_t *outpkt,
+                         uint8_t id_hi, uint8_t id_lo)
+{
+    const char *os = "riot";
+    size_t len = strlen("riot");
+
+    memcpy(response, os, len);
+
+    return coap_make_response(scratch, outpkt, (const uint8_t *)response, len,
+                              id_hi, id_lo, &inpkt->tok, COAP_RSPCODE_CONTENT,
+                              COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
 static int handle_get_board(coap_rw_buffer_t *scratch,
                             const coap_packet_t *inpkt,
                             coap_packet_t *outpkt,
@@ -177,7 +229,6 @@ static int handle_get_ribot(coap_rw_buffer_t *scratch,
                               id_hi, id_lo, &inpkt->tok, COAP_RSPCODE_CONTENT,
                               COAP_CONTENTTYPE_TEXT_PLAIN);
 }
-
 
 static void parse_message(int16_t * dir_speed_ratio, int16_t * angle_speed_ratio, int16_t * crc)
 {
